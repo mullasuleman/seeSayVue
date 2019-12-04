cf3bbac0d64ba11fb1b68601e7a408b09dd9d82c
Mike Mueller <muellerm.dev@gmail.com>
2019-12-04 15:26:24 -0500

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Header">
+    <h1>SEE&SAY</h1>
+    <nav></nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {}
+};
+</script>
+
+<style scoped>
+.Header {
+  width: 100vw;
+  height: 68px;
+}
+</style>
