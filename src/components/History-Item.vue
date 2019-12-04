9486ea4ea670bd14de7f0cb8b28f5a410642d5f8
Mike Mueller <muellerm.dev@gmail.com>
2019-12-04 16:32:25 -0500

    add history components

---
+++ src/components/History-Item.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="HistoryItem">
+    <h3>Item</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HistoryItem"
+};
+</script>
+
+<style>
+#HistoryItem {
+}
+</style>
---
+++ src/components/HistoryBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="HistoryBar">
+    <h3>History</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HistoryBar"
+};
+</script>
+
+<style>
+#HistoryBar {
+}
+</style>
