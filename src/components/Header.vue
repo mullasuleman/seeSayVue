979a3e947d5e02b53ff3017b8c30d81dc42646db
Usman Butt <usman.butt@dcmail.ca>
2019-12-09 13:33:04 -0500

    Updated Footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,26 @@
 <template>
-  <div class="Header">
-    <h1>SEE&SAY</h1>
-  </div>
+	<div class="Header">
+		<h1>SEE&SAY</h1>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Header",
-  components: {},
-  props: {
-    history: Array
-  }
-};
+	export default {
+		name: "Header"
+	};
 </script>
 
 <style scoped>
-.Header {
-  padding: 0.2rem 8rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100vw;
-  height: 10vh;
-  background-color: rgb(250, 198, 28);
-  font-family: "Cinzel", serif;
-  color: #fff;
-}
+	.Header {
+		padding: 0.2rem 8rem;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		width: 100vw;
+		height: 10vh;
+		background-color: rgb(250, 198, 28);
+		font-family: "Cinzel", serif;
+		color: #fff;
+	}
 </style>
